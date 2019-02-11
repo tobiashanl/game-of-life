c7069ae662e879fb3a6c6eff598fcda1d4277ce8
Tobias Hanl <tobias@Tobiass-MacBook-Pro.local>
2019-02-11 23:21:02 +0100

    Create Board

---
+++ src/components/Board.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="updateBoard">Generate new board</button>
+    <table>
+      <row v-for="(row, index) in board" :key="index" :row="row"/>
+    </table>
+  </div>
+</template>
+
+<script>
+import Row from "./Row.vue";
+
+export default {
+  name: "Board",
+  components: {
+    Row
+  },
+  data() {
+    return {
+      board: this.generateBoard(),
+    };
+  },
+  methods: {
+    updateBoard() {
+      this.board = this.generateBoard();
+    },
+    generateBoard() {
+      const board = [];
+      for (let i = 0; i < 200; i++) {
+        const row = [];
+        for (let j = 0; j < 200; j++) {
+          row.push(Math.random() < 0.95 ? 0 : 1);
+        }
+        board.push(row);
+      }
+      return board;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+    width: 2000px;
+}
+</style>
---
+++ src/components/Row.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <td
+      v-for="(col, index) in row"
+      :key="index"
+      :class="{ live: col === 1 }"
+    ></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Row",
+  props: ["row"]
+};
+</script>
+
+<style scoped>
+td {
+  width: 10px;
+  height: 10px;
+}
+.live {
+  background-color: black;
+}
+</style>
